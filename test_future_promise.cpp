004e8e5d4b86994f687bc52cf036f152c09746ec
sophiaho26 <50991627+sophiaho26@users.noreply.github.com>
2020-12-04 23:23:42 +0800

    Add files via upload

--- test_future_promise.cpp
+++ test_future_promise.cpp
@@ -23,8 +23,8 @@ enum State {
 	kUnstable
 };
 
-enum RC {
-	kOK = 0,
+enum common {
+	kAudienceOk = 0,
 	kTimeout,
 	kFail
 };
@@ -32,21 +32,62 @@ enum RC {
 std::mutex m;
 std::condition_variable cv;
 bool finished = false;
-using SATRT_COMPLETE_FUNC = std::function<void(const std::string&, const RC&, const State&)>;
+using SATRT_COMPLETE_FUNC = std::function<void(const std::string&, const common&, const State&)>;
 
-void OnStartComplete(const std::string& name, const RC& rc, const State& state) {
+void OnStartComplete(const std::string& name, const common& rc, const State& state) {
 	std::time_t start_time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 	std::cout << name << ": time now: " << std::ctime(&start_time) << std::endl;
-
 	std::cout << name << ":rc = " << rc << std::endl;
 	std::cout << name << ":state = " << state << std::endl;
 	finished = true;
 }
 
-class StreamSession {
+struct ExitAndExecuteSignals {
+	std::promise<void> exit_signal;
+	std::future<void> exit_future = exit_signal.get_future();
+	std::promise<common> execute_signal;
+	std::future<common> execute_future = execute_signal.get_future();
+};
+
+
+template <typename Func, typename Obj>
+common WailUntil(Func func, Obj obj,std::string name,
+	std::chrono::system_clock::time_point& time_out_seconds) {
+	auto rc = common::kAudienceOk;
+	ExitAndExecuteSignals signals;
+
+	std::unique_ptr<std::thread>th1_ = std::make_unique<std::thread>(func, obj, std::move(signals.exit_future), std::move(signals.execute_signal));
+
+	if (std::future_status::ready == signals.execute_future.wait_until(time_out_seconds))
+	{
+		std::cout << name << ":Run Func Finished. \n";
+	}
+	else
+	{
+		std::cout << name << ":Run Func did not complete!\n";
+		rc = common::kTimeout;
+		signals.exit_signal.set_value();
+		if (th1_->joinable()) {
+			th1_->join();
+		}
+		return rc;
+	}
+	if (th1_->joinable()) {
+		th1_->join();
+	}
+
+	rc = signals.execute_future.get();
+	if (rc != common::kAudienceOk) {
+		std::cout << name << ":Func return rc: " << rc << " \n";
+	}
+	return rc;
+}
+
+
+class BaseStreamSession {
 public:
-	StreamSession(std::string name) { name_ = name; }
-	~StreamSession() {}
+	BaseStreamSession(std::string name) { name_ = name; }
+	~BaseStreamSession() {}
 
 	void Init() {
 		if (state_ != kInactive) return;
@@ -62,23 +103,17 @@ public:
 			api_thread_.join();
 		}
 	}
-	void Start(StreamSession* session, int timeout, SATRT_COMPLETE_FUNC funt_ptr) {
+	void StartCanTerminate(BaseStreamSession* session, int timeout, SATRT_COMPLETE_FUNC funt_ptr) {
 		if (state_ != kInited) return;
 		state_ = kStarting;
-		api_thread_ = std::thread(&StreamSession::StartTask, this, funt_ptr, timeout);
-		return;
-	}
-	void StartCanTerminate(StreamSession* session, int timeout, SATRT_COMPLETE_FUNC funt_ptr) {
-		if (state_ != kInited) return;
-		state_ = kStarting;
-		std::thread t = std::thread(&StreamSession::StartTaskCanTerminate, this, funt_ptr, timeout);
+		std::thread t = std::thread(&BaseStreamSession::StartTaskCanTerminate, this, funt_ptr, timeout);
 		t.join();
 		return;
 	}
 
-	void UpdateTrackInfo(StreamSession* session, int timeout, SATRT_COMPLETE_FUNC funt_ptr) {
+	void UpdateTrackInfo(BaseStreamSession* session, int timeout, SATRT_COMPLETE_FUNC funt_ptr) {
 		if (state_ != kUpdateNeeded && state_ != kUnstable) return;
-		api_thread_ = std::thread(&StreamSession::StartTaskCanTerminate, this, funt_ptr, timeout);
+		api_thread_ = std::thread(&BaseStreamSession::StartTaskCanTerminate, this, funt_ptr, timeout);
 		return;
 	}
 private:
@@ -97,7 +132,7 @@ private:
 		std::cout << name_ << ":CreateChannel Thread End" << std::endl;
 	}
 
-	void RunCreateChannelCanTerminate(std::future<void> future_obj, std::promise<void> p) {
+	void RunCreateChannelCanTerminate(std::future<void> future_obj, std::promise<common> p) {
 		std::cout << name_ << ":RunCreateChannelCanTerminate Thread Start" << std::endl;
 		int i = 0;
 		while (future_obj.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout && (++i < 3)) {
@@ -108,22 +143,29 @@ private:
 		ramdon = 2;
 		std::cout << name_ << ":RunCreateChannelCanTerminate Thread End" << std::endl;
 		std::cout.flush();
-		p.set_value();
+		if (i == 3)
+			p.set_value(common::kAudienceOk);
+		else
+			p.set_value(common::kTimeout);
 	}
 
-	void RunSingalingConnectCanTerminate(std::future<void> future_obj, std::promise<void> p) {
+	void RunSingalingConnectCanTerminate(std::future<void> future_obj, std::promise<common> p) {
 		std::cout << name_ << ":RunSingalingConnectCanTerminate Thread Start" << std::endl;
 		int i = 0;
 		while (future_obj.wait_for(std::chrono::milliseconds(1)) == std::future_status::timeout && (++i < 7)) {
-			std::this_thread::sleep_for(std::chrono::milliseconds(3000));
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
 			std::cout << name_ << ":RunSingalingConnectCanTerminate" << std::endl;
 		}
 
 		// 	p1.set_value_at_thread_exit(6);
 		ramdon = 6;
 		std::cout << name_ << ":RunSingalingConnect Thread End" << std::endl;
-		p.set_value();
+		std::cout.flush();
 
+		if (i == 7)
+			p.set_value(common::kAudienceOk);
+		else
+			p.set_value(common::kTimeout);
 	}
 	void RunSingalingConnect(std::promise<int> p) {
 		std::cout << name_ << ":RunSingalingConnect Thread Start" << std::endl;
@@ -135,10 +177,8 @@ private:
 
 	}
 
-
-
-	void StartTask(SATRT_COMPLETE_FUNC funt_ptr, int timeout) {
-		RC rc = RC::kFail;
+	void StartTaskCanTerminate(SATRT_COMPLETE_FUNC funt_ptr, int timeout) {
+		common rc = common::kAudienceOk;
 		std::chrono::system_clock::time_point time_out_seconds
 			= std::chrono::system_clock::now() + std::chrono::seconds(timeout);
 
@@ -146,169 +186,87 @@ private:
 		std::time_t start_time = std::chrono::system_clock::to_time_t(start);
 		std::cout << name_ << ":time now: " << std::ctime(&start_time) << std::endl;
 
-		std::promise<int> p1;
-		std::future<int> f_completes = p1.get_future();
+// 		rc = CreatStreamChannelTask(time_out_seconds, funt_ptr);
+		rc = WailUntil(&BaseStreamSession::RunCreateChannelCanTerminate, this, name_, time_out_seconds);
+		if (rc != kAudienceOk) return;
 
-		std::thread th1_ = std::thread(&StreamSession::RunCreateChannel, this, std::move(p1));
-		th1_.detach();
-		// 		std::thread([](std::promise<int> p1)
-		// 			{
-		// 				std::cout << "CreateChannel Thread Start" << std::endl;
-		// 				std::this_thread::sleep_for(std::chrono::seconds(1));
-		// 				// 	p1.set_value_at_thread_exit(9);
-		// 				p1.set_value(5);
-		// 				std::cout << "CreateChannel Thread End" << std::endl;
-		// 			},
-		// 			std::move(p1)
-		// 				).detach();
-
-
-		if (std::future_status::ready == f_completes.wait_until(time_out_seconds))
-		{
-			std::cout << name_ << ":RunCreateChannel: " << f_completes.get() << " Completed.\n";
-		}
-		else
-		{
-			std::cout << name_ << ":RunCreateChannel did not complete!\n";
-			rc = RC::kTimeout;
-			funt_ptr(name_, rc, state_);
-			return;
-		}
-
-		std::promise<int> p2;
-		std::future<int> f_times_out = p2.get_future();
-
-		// 			std::thread([](std::promise<int> p2)
-		// 				{
-		// 					std::cout << "RunSingalingConnect Thread Start" << std::endl;
-		// 					std::this_thread::sleep_for(std::chrono::seconds(2));
-		// 					// 	p1.set_value_at_thread_exit(9);
-		// 					p2.set_value(3);
-		// 					std::cout << "RunSingalingConnect Thread End" << std::endl;
-		// 				},
-		// 				std::move(p2)
-		// 					).detach();
-
-		std::thread th2_ = std::thread(&StreamSession::RunSingalingConnect, this, std::move(p2));
-		th2_.detach();
-		if (std::future_status::ready == f_times_out.wait_until(time_out_seconds))
-		{
-			std::cout << name_ << ":RunSingalingConnect: " << f_times_out.get() << " Completed.\n";
-		}
-		else
-		{
-			std::cout << name_ << ":RunSingalingConnect did not complete!\n";
-			rc = RC::kTimeout;
-			funt_ptr(name_, rc, state_);
-			return;
-		}
-
-		rc = RC::kOK;
-		state_ = KReady;
-		funt_ptr(name_, rc, state_);
-		return;
+		rc = WailUntil(&BaseStreamSession::RunSingalingConnectCanTerminate, this, name_, time_out_seconds);
+// 		rc = ConnectAndUpdateToSignalingServerTask(time_out_seconds, funt_ptr);
+		if (rc != kAudienceOk) { 
+			std::cout << name_ << ":rc: " << rc << std::endl;
+			return; }
 	}
 
-	void StartTaskCanTerminate(SATRT_COMPLETE_FUNC funt_ptr, int timeout) {
-		RC rc = RC::kFail;
-		std::chrono::system_clock::time_point time_out_seconds
-			= std::chrono::system_clock::now() + std::chrono::seconds(timeout);
 
-		auto start = std::chrono::system_clock::now();
-		std::time_t start_time = std::chrono::system_clock::to_time_t(start);
-		std::cout << name_ << ":time now: " << std::ctime(&start_time) << std::endl;
+	common CreatStreamChannelTask(
+		std::chrono::system_clock::time_point &time_out_seconds, SATRT_COMPLETE_FUNC funt_ptr) {
+		auto rc = common::kAudienceOk;
+		ExitAndExecuteSignals create_channel_signals;
+		std::unique_ptr<std::thread>th1_ = std::make_unique<std::thread>(&BaseStreamSession::RunCreateChannelCanTerminate, this, std::move(create_channel_signals.exit_future), std::move(create_channel_signals.execute_signal));
 
-		std::promise<void> exit_signal_1;
-		std::future<void> exit_future_1 = exit_signal_1.get_future();
-		std::promise<void> execute_promise_1;
-		std::future<void> execute_future_1 = execute_promise_1.get_future();
-
-		std::unique_ptr<std::thread>th1_ = std::make_unique<std::thread>(&StreamSession::RunCreateChannelCanTerminate, this, std::move(exit_future_1), std::move(execute_promise_1));
-
+		if (std::future_status::ready == create_channel_signals.execute_future.wait_until(time_out_seconds))
+		{
+			std::cout << name_ << ":RunCreateChannelCanTerminate Finished: \n";
+		}
+		else
+		{
+			std::cout << name_ << ":RunCreateChannelCanTerminate did not complete!\n";
+			rc = common::kTimeout;
+			create_channel_signals.exit_signal.set_value();
+			if (th1_->joinable()) {
+				th1_->join();
+			}
+			funt_ptr(name_, rc, state_);
+			return rc;
+		}
 		if (th1_->joinable()) {
 			th1_->join();
 		}
 
-		// 		std::thread([](std::promise<int> p1)
-		// 			{
-		// 				std::cout << "CreateChannel Thread Start" << std::endl;
-		// 				std::this_thread::sleep_for(std::chrono::seconds(1));
-		// 				// 	p1.set_value_at_thread_exit(9);
-		// 				p1.set_value(5);
-		// 				std::cout << "CreateChannel Thread End" << std::endl;
-		// 			},
-		// 			std::move(p1)
-		// 				).detach();
-
-
-		if (std::future_status::ready == execute_future_1.wait_until(time_out_seconds))
-		{
-			std::cout << name_ << ":RunCreateChannel Finished: \n";
+		rc = create_channel_signals.execute_future.get();
+		if (rc != common::kAudienceOk) {
+			std::cout << name_ << ":RunCreateChannel Fail \n";
 		}
-		else
-		{
-			std::cout << name_ << ":RunCreateChannel did not complete!\n";
-			rc = RC::kTimeout;
-			exit_signal_1.set_value();
-			// 			th1_.reset();
-			funt_ptr(name_, rc, state_);
-			// 			if (th1_.joinable()) {
-			// 				th1_.join();
-			// 			}
-			return;
-		}
-		// 		if (th1_.joinable()) {
-		// 			th1_.join();
-		// 		}
+		return rc;
+	}
 
-		std::promise<void> exit_signal_2;
-		std::future<void> exit_future_2 = exit_signal_2.get_future();
-		std::promise<void> execute_promise_2;
-		std::future<void> execute_future_2 = execute_promise_2.get_future();
+	common ConnectAndUpdateToSignalingServerTask(
+		std::chrono::system_clock::time_point time_out_seconds, SATRT_COMPLETE_FUNC funt_ptr) {
+		auto rc = common::kAudienceOk;
+		ExitAndExecuteSignals connect_signaling_signals;
 
-		// 			std::thread([](std::promise<int> p2)
-		// 				{
-		// 					std::cout << "RunSingalingConnect Thread Start" << std::endl;
-		// 					std::this_thread::sleep_for(std::chrono::seconds(2));
-		// 					// 	p1.set_value_at_thread_exit(9);
-		// 					p2.set_value(3);
-		// 					std::cout << "RunSingalingConnect Thread End" << std::endl;
-		// 				},
-		// 				std::move(p2)
-		// 					).detach();
+		std::unique_ptr<std::thread> th2_ = std::make_unique<std::thread>(&BaseStreamSession::RunSingalingConnectCanTerminate, this, std::move(connect_signaling_signals.exit_future), std::move(connect_signaling_signals.execute_signal));
+// 		if (th2_->joinable()) {
+// 			th2_->join();
+// 		}
 
-		std::unique_ptr<std::thread> th2_ = std::make_unique<std::thread>(&StreamSession::RunSingalingConnectCanTerminate, this, std::move(exit_future_2), std::move(execute_promise_2));
-
-		// 		if (th2_->joinable()) {
-		// 			th2_->join();
-		// 		}
-
-		if (std::future_status::ready == execute_future_2.wait_until(time_out_seconds))
+		if (std::future_status::ready == connect_signaling_signals.execute_future.wait_until(time_out_seconds))
 		{
 			std::cout << name_ << ":RunSingalingConnect: Finished: " << "\n";
 		}
 		else
 		{
 			std::cout << name_ << ":RunSingalingConnect did not complete!\n";
-			rc = RC::kTimeout;
-			exit_signal_2.set_value();
+			rc = common::kTimeout;
+			connect_signaling_signals.exit_signal.set_value();
+
 			if (th2_->joinable()) {
 				th2_->join();
-			}
-			th2_.reset();
+				}
 			funt_ptr(name_, rc, state_);
-			return;
+			return rc;
 		}
 		if (th2_->joinable()) {
 			th2_->join();
 		}
 
-		rc = RC::kOK;
-		state_ = KReady;
+		rc = connect_signaling_signals.execute_future.get();
+		if (rc != common::kAudienceOk) {
+			std::cout << name_ << ":RunSingalingConnect Failed \n";
+		}
 		funt_ptr(name_, rc, state_);
-		return;
+		return rc;
 	}
-
 
 	bool inited_{ false };
 	std::thread api_thread_;
@@ -330,16 +288,16 @@ int main()
 	std::cout << "end time: " << std::chrono::system_clock::to_time_t(time_out_seconds) << std::endl;
 
 
-	StreamSession s1("s1");
+	BaseStreamSession s1("s1");
 	s1.Init();
-	StreamSession s2("s2");
+	BaseStreamSession s2("s2");
 	s2.Init();
 
-	RC rc;
+	common rc;
 	State state;
 
 	s1.StartCanTerminate(&s1, wait_seconds, &OnStartComplete);
-	auto lambda = [&s1, &wait_seconds](StreamSession s1, int wait_seconds) {
+	auto lambda = [&s1, &wait_seconds](BaseStreamSession s1, int wait_seconds) {
 		s1.StartCanTerminate(&s1, wait_seconds, &OnStartComplete);
 	};
 
@@ -357,11 +315,11 @@ int main()
 	std::cout << "time now: " << std::ctime(&start_time) << std::endl;
 
 	finished = false;
-	wait_seconds = 4;
+	wait_seconds = 3;
 	start = std::chrono::system_clock::now();
 	time_out_seconds
 		= start + std::chrono::seconds(wait_seconds);
-	s2.StartCanTerminate(&s2, 2, &OnStartComplete);
+	s2.StartCanTerminate(&s2, 1, &OnStartComplete);
 	// 		s2.Start(&s2, 2, &OnStartComplete);
 
 	if (cv.wait_until(lk, time_out_seconds, []() {return finished == true; })) {
